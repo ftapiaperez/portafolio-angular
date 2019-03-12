03d03a45c6da1433b48268a3cd09170479e3eda3
Francisco Tapia <fjtp1407@gmail.com>
2019-03-12 10:39:46 -0300

    Fin Seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemsComponent } from './pages/items/items.component';
 
-const routes: Routes = [];
+
+const app_routes: Routes = [
+{ path: 'home', component: PortafolioComponent},
+{ path: 'about', component: AboutComponent},
+{ path: 'items', component: ItemsComponent},
+{ path: '**', pathMatch: 'full', redirectTo: 'home' }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(app_routes, { useHash: true})
+    ],
+    exports: [
+        RouterModule
+    ]
+
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemsComponent } from './pages/items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
